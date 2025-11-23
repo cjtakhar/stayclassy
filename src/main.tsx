e87bc2f9acb9f2e75cfc096405f972f373d9ddb8
cjtakhar <cjtakhar@gmail.com>
2025-11-23 15:29:05 -0500

    classy ai

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* basename should match your GitHub repo name */}
+    <BrowserRouter basename="/stayclassy">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
